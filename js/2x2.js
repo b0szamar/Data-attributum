9438e481d01d5a630b4091d1ae34094770d65994
2020b-Szabó Marcell <74899044+b0szamar@users.noreply.github.com>
2023-11-17 11:15:20 +0100

    Add files via upload

---
+++ js/2x2.js
@@ -0,0 +1,7 @@
+
+function changeColor(color) {
+    document.body.style.backgroundColor = color;
+}
+window.addEventListener("load",function() { changeColor('red') })
+let index = document.getElementById("item").dataset.indexNumber;
+
